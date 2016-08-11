a221a16b4d9a038a9bfdeea2cd6ea01383d47608
Christoph M. Wintersteiger <cwinter@microsoft.com>
2016-08-11 12:55:16 +0100

    Added debug output to custom tactic.

--- src/tactic/custom/custom_tactic.cpp
+++ src/tactic/custom/custom_tactic.cpp
@@ -30,32 +30,80 @@ Notes:
 #include"qfbv_tactic.h"
 
 
+class print_info_tactic : public skip_tactic {
+    tactic * m_t;
+    unsigned m_lvl;
+    std::string m_msg;
+
+public:
+    print_info_tactic(tactic * t, unsigned lvl, const char * msg) : m_t(t), m_lvl(lvl), m_msg(msg) {
+        SASSERT(m_t);
+        m_t->inc_ref();
+    }
+
+    virtual void operator()(goal_ref const & in,
+        goal_ref_buffer & result,
+        model_converter_ref & mc,
+        proof_converter_ref & pc,
+        expr_dependency_ref & core) {
+        tactic_exception * te = 0;
+
+        IF_VERBOSE(m_lvl, verbose_stream() << "(start " << m_msg << ")\n";);
+
+        try {
+            (*m_t)(in, result, mc, pc, core);
+        }
+        catch (tactic_exception ex) {
+            te = &ex;            
+        }
+
+        if (is_decided_sat(result))
+            std::cout << "sat" << std::endl;
+        else if (is_decided_unsat(result))
+            std::cout << "unsat" << std::endl;
+        else
+            std::cout << "unknown" << std::endl;
+
+        IF_VERBOSE(m_lvl, verbose_stream() << "(end " << m_msg << ")\n";);
+        if (te) throw * te;
+    }
+};
+
+tactic * mk_print_info_tactic(tactic * t, unsigned lvl, const char * msg) {
+    return alloc(print_info_tactic, t, lvl, msg);
+}
+
+
 tactic * mk_custom_tactic(ast_manager & m, params_ref const & p) {
-// #ifdef USE_ARRAY_TH
-// #define SMT_THEORY "QF_AUFBV"
-//   s = Z3_mk_solver_for_logic(ctx(), Z3_mk_string_symbol(ctx(), SMT_THEORY));
-//   Z3_solver_inc_ref(ctx(), s);
-// #endif
-  
-  params_ref prms = p;
-  prms.set_bool("elim_and", true);
-  prms.set_bool("blast_distinct", true);
-  prms.set_str("smt.logic", "QF_UFBV");  
-  
-  tactic * st = using_params(
-      and_then(mk_simplify_tactic(m),
-          mk_propagate_values_tactic(m),
-          //mk_bv_bounds_tactic(m),
-          mk_solve_eqs_tactic(m),
-          mk_elim_uncnstr_tactic(m),
-          mk_reduce_args_tactic(m),
-          mk_simplify_tactic(m),
-          mk_bv_size_reduction_tactic(m),
-          mk_max_bv_sharing_tactic(m),
-          //mk_ackermannize_bv_tactic(m,p),
-          cond(mk_is_qfbv_probe(), mk_qfbv_tactic(m, p), mk_smt_tactic())),
-      prms);
-  
-  st->updt_params(p);
-  return st;
+    // #ifdef USE_ARRAY_TH
+    // #define SMT_THEORY "QF_AUFBV"
+    //   s = Z3_mk_solver_for_logic(ctx(), Z3_mk_string_symbol(ctx(), SMT_THEORY));
+    //   Z3_solver_inc_ref(ctx(), s);
+    // #endif
+
+    params_ref prms = p;
+    prms.set_bool("elim_and", true);
+    prms.set_bool("blast_distinct", true);
+    prms.set_str("smt.logic", "QF_UFBV");
+
+    tactic * st =
+        mk_print_info_tactic(
+            using_params(
+                and_then(
+                    and_then(mk_simplify_tactic(m),
+                        mk_propagate_values_tactic(m),
+                        //mk_bv_bounds_tactic(m),
+                        mk_solve_eqs_tactic(m),
+                        mk_elim_uncnstr_tactic(m),
+                        mk_reduce_args_tactic(m),
+                        mk_simplify_tactic(m),
+                        mk_bv_size_reduction_tactic(m)),
+                    and_then(
+                        mk_max_bv_sharing_tactic(m),
+                        cond(mk_is_qfbv_probe(), mk_qfbv_tactic(m, p), mk_smt_tactic(p)))
+                ), prms),
+            2, "custom");
+
+    st->updt_params(p);
+    return st;
 }
